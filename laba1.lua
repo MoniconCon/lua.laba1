b33d1f07201fb07db50980639759eec799fecc77
MoniconCon <monicon78@gmail.com>
2019-10-24 10:26:03 +0300

    add scrin lua

---
+++ laba1.lua
@@ -0,0 +1,15 @@
+lgi  = require 'lgi'
+
+gtk = lgi.Gtk
+gtk.init()
+
+bld = gtk.Builder()
+bld:add_from_file('winl1.glade')
+
+ui =  bld.objects
+
+ui.window.title = 'laba 1'
+ui.window.on_destroy = gtk.main_quit
+ui.window:show_all()
+
+gtk.main()
